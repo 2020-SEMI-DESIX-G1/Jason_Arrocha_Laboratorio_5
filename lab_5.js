b74f06d87f06e0b0922a749f0b2a8d18bb73b572
zangetsu507 <jason.x.arrocha@gmail.com>
2020-06-17 23:48:31 -0500

    Initial commit.

---
+++ lab_5.js
@@ -0,0 +1,30 @@
+if(parte1=true){
+    var a=5;
+    var b=5;
+        console.log('El resultado de la suma es:'+(a + b));
+        console.log('El resultado de la resta es:' +(a - b));
+        console.log('El resultado de la multiplicación es:'+ (a * b));
+        console.log('El resultado de la división es:'+(a / b));
+}
+
+if(parte2=true){
+    let a="Mi nombre es: ";
+    let b="Jason Arrocha";
+        console.log(a + b);
+}
+
+if(parte3=true){
+    const a=4;
+    const b="Jason";
+        console.log("El tipo de dato de a es:"+ typeof a);
+        console.log("El tipo de dato de b es:"+ typeof b);
+}
+
+if(parte4=true){
+    var objeto = new Object();
+    objeto.numero = 1;
+    objeto.cadena = "Hola";
+    objeto.booleano = true;
+    objeto.vacio = "";
+    console.log(objeto);
+}
